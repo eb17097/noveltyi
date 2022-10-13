e8b6b2e593bf800df0c41301ee9840f4f46a56cc
dmitry992 <dima.boch4@gmail.com>
2022-10-13 12:06:25 +0500

    ready page "contacts"

--- js/page_tabs.js
+++ js/page_tabs.js
@@ -1,6 +1,6 @@
 // Variables for popup
-let tab_items = document.querySelectorAll('.digital__future-content__top-item');
-let tab_content = document.querySelectorAll('.digital__future-content__item');
+let tab_items = document.querySelectorAll('.page__tabs-item');
+let tab_content = document.querySelectorAll('.tabs__content-item');
 
 
 
@@ -13,15 +13,15 @@ let tab_switch = function(){
     }
 
     for (let index = 0; index < tab_content.length; index++) {  
-        let tab_index = this.getAttribute('data-index');
-        this.classList.add('active');
-        tab_content[tab_index].classList.add('active');
+        if(this.getAttribute('data-index') == tab_content[index].getAttribute('data-index')){
+            this.classList.add('active');
+            tab_content[index].classList.add('active');
+        }
     }
 
 }
 
 for (let index = 0; index < tab_items.length; index++) {
-    const element = tab_items[index];
-    
+    const element = tab_items[index]; 
     element.addEventListener('click', tab_switch, false);
 }
